af371a7f56e7c144e6f2e9ca8352ce7eea62c492
maxime <maxime.aumegeas@laplateforme.io>
2025-10-28 16:22:20 +0100

    creation d'un jeu de dés v3 v1 et 2 fini

--- src/JeuDeDesVersion2.java
+++ src/JeuDeDesVersion2.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
-public class JeuDeDesVersion1 {
+public class JeuDeDesVersion2 {
     public static void main(String[] args) {
 
         int a = ((int) (Math.random() * 6));
         int b = ((int) (Math.random() * 6));
-
         int c = (a + b);
+
+        boolean run = true;
         System.out.println("trouver le nombre entre 0 et 12?");
         Scanner entrer = new Scanner(System.in);
         int d = entrer.nextInt();
 
-        if (c == d) {
-            System.out.println("Bravo");
-        }
-        else {
-            System.out.println("Perdu");
+        while (run) {
+            if (c == d) {
+                System.out.println("You Win");
+                run = false;
+            } else {
+                System.out.println("You Lose!! try again!!");
+                d = entrer.nextInt();
+            }
         }
     }
-
-
 }
